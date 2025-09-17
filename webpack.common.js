39cad995f4c5796e388a03474e1f3b4584e5232b
nikitafloy <nikita5051@yandex.ru>
2025-09-17 09:21:52 +0800

    add accordion

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './js/app.js',
+    accordion: './js/accordion.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      inject: "body",
+      chunks: ['app', 'accordion'],
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './js/app.js',
+    filename: './js/[name].js',
   },
 };
